df36473276c4bd14c272c2ab4d2ae73266cdfcff
Lucas Meyer <22614524@sun.ac.za>
2021-08-31 13:38:51 +0200

    Added Standardization

--- src/neural-net.py
+++ src/neural-net.py
@@ -1,59 +1,45 @@
 from sklearn.neural_network import MLPRegressor
+from sklearn.preprocessing import StandardScaler
 import numpy as np
 
-# labels of original 7 features and 3 outputs
-labels = [["RunOrder"],
-        ["Temperature"],
-        ["Speed"],
-        ["Thickness of Layer"],
-        ["Extrusion Width"],
-        ["Test Tube Position"],
-        ["Internal Fill Angle"],
-        ["Young(GPa)"],
-        ["Break in Tension(MPa)"],
-        ["Breakage Deformation"]]
+# INPUT
+my_input = open('printer_runs.txt','r').read().split('\n')
+A = [] 
+
+for line in my_input:
+  for a in line.split(","):
+    A.append(a)
+
+labels = A[0:9]
+X = np.array(A[9:585]).reshape(-1, 9).astype(np.float64)
 
 
-# read in input file
-input_file = open("run_orders.txt", "r")
-runs = input_file.read()
-run_list = runs.split(" ")
-input_file.close()
+
+# STANDARDIZING INPUT - must still normalize
+x_train = X[:54,:-3]
+y_train = X[0:54:,7]
+x_test = X[54:,:-3]
+y_test = X[54:,7]
+
+scale = StandardScaler(with_mean=0, with_std=1).fit(x_train, y_train)
+train_norm = scale.transform(x_train)
+test_norm = scale.transform(x_test)
 
 
-# vertical/horizontal constants
-vert = 1
-hor = 0
 
-
-# fixup input
-for i in range(64):
-  run_list[10*i]=i+1
-  idx = 10*i+5
-  if run_list[idx] == "Vertical":
-    run_list[idx] = vert
-  else:
-    run_list[idx] = hor
-
-A = np.array(run_list[0:640])
-B = np.reshape(A, (-1, 10))
-X = B[:,1:]
-
-x_labels = B[:,:1]
-x_train = X[:50,:-3].astype(float)
-y_train = X[:50,-1:].reshape(50).astype(float)
-x_test = X[50:,:-3].astype(float)
-y_test = X[50:,-1:].reshape(14).astype(float)
-
-
-# Neural Network of 50 samples and 14 tests
-NN = MLPRegressor(max_iter=3000).fit(x_train, y_train)
-for i in range(14):
+# Neural Network of 54 samples and 10 tests
+NN = MLPRegressor(max_iter=10000).fit(train_norm, y_train)
+for i in range(10):
   print("Test "+str(i+1))
-  print(NN.predict(x_test[i].reshape(1,-1)))
-  
-  # Haven't figured out why, but the predict is very inconsistent 
-  # and not close to the tests
-  
+  print(NN.predict(test_norm[i].reshape(1,-1)))
   print(y_test[i])
   print("")
+
+
+
+# testing one of features as example
+print("Testing: " + labels[2])
+test_a = scale.transform(np.array([180,60,0.1,0.6,0,1]).reshape(1,-1))
+test_b = scale.transform(np.array([180,60,0.5,0.6,0,1]).reshape(1,-1))
+print("0.1: "+str(NN.predict(test_a)))
+print("0.5: "+str(NN.predict(test_b))) #thicc boi gives better score
