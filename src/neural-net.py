c37102eaa3b33273aeb1e1bc94cb2ba6197a9909
Lucas Meyer <22614524@sun.ac.za>
2021-09-29 13:05:35 +0200

    Added more NN stuff

--- src/neural-net.py
+++ src/neural-net.py
@@ -37,15 +37,15 @@ class NN:
 			if (data_types[i] == "D"):
 				i_d += 1
 				x_list.append(A[:,i])
-				self.x_labels.append("D:"+labels[i])
+				self.x_labels.append("D:" + labels[i])
 			elif (data_types[i] == "C"):
 				i_c += 1
 				x_list.append(A[:,i])
-				self.x_labels.append("C:"+labels[i])
+				self.x_labels.append("C:" + labels[i])
 			elif (data_types[i] == "O"):
 				i_o += 1
 				y_list.append(A[:,i])
-				self.y_labels.append("O:"+labels[i])
+				self.y_labels.append("O:" + labels[i])
 
 
 		#Normalization
@@ -65,16 +65,20 @@ class NN:
 					X_list.append(x)
 					self.X_labels.append("D:" + labels[i] + " = " + dis)
 				mean_list.pop()
-				labels[i] = str(len(disc_vals)-1) + labels[i]
-			elif (data_types[i] == "C"): 
-				#Continuous Normalization
-				print("cts todo")
-		
-		self.mean = np.zeros(len(mean_list))
-		for i in range(len(mean_list)):
-			self.mean[i] = mean_list[i]
+				#labels[i] = str(len(disc_vals)-1) + labels[i]
+			elif (data_types[i] == "C"):
+				#Continous Standardization
+				x = np.array(x_list[i]).astype(np.float64)
+				x = (x - x.mean()) / (x.std())
+				X_list.append(x)
+				mean_list.append(x.mean())
+				self.X_labels.append("C:" + labels[i])
+				print(x)
+
+
 
 		# Convert lists
+		self.mean = np.array(mean_list)
 		d = len(X_list)
 		X = np.zeros((N, d))		
 		for i in range(d):
@@ -100,6 +104,7 @@ class NN:
 			print("Usage: NN.score(feature_idx, output_idx)")
 			return -1
 
+		
 		if (self.x_labels[feature].split(":")[0] == "D"):
 			i_list = []
 			a = self.mean
@@ -108,28 +113,34 @@ class NN:
 				if self.x_labels[feature] in self.X_labels[i]:
 					i_list.append(i)
 			
-			
 			for i in i_list:
 				a[i] = 0
 
-			
 			for i in i_list:
 				a[i] = 1
 				ret.append(self.X_labels[i])
 				ret.append(self.nn[output].predict(a.reshape(1,-1)))
 				a[i] = 0
 
+			return ret
+
 		elif (self.x_labels[feature].split(":")[0] == "C"):
-			print("cts todo")
+			a = self.mean
 			
+			for i in range(len(self.X_labels)):
+				if self.x_labels[feature] in self.X_labels[i]:
+					return a[i]
 
-		return ret
 
+test = NN('input/SURFACE_RESPONSE_RUNS.csv')
+test.fit()
+print(test.score(0,0))
+quit()
 
-test = NN('FRACTIONAL_FACTORIAL_RUNS.csv')
+test = NN('input/FRACTIONAL_FACTORIAL_RUNS.csv')
 test.fit()
 print(test.score(2,0))
 
-test = NN('3D_PRINTER_RUNS.csv')
+test = NN('input/3D_PRINTER_RUNS.csv')
 test.fit()
 print(test.score(2,0))
