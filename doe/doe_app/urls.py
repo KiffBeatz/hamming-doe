74f38eaf6979fd88c2ce6113530a0ed050c2c33e
21660328 <21660328@sun.ac.za>
2021-11-01 10:08:49 +0200

    Moved File format to dataset page

--- doe/doe_app/urls.py
+++ doe/doe_app/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('graph/', views.graph, name='graph'),
     path('datasets/', views.datasets, name='datasets'),
     path('datasets/view', views.view, name='view'),
-    path('datasets/format', views.format, name='format'),
     #path('stores/<int:store_id>/',stores_views.detail),
 ]
